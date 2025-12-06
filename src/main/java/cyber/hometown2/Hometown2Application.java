b3c8fff8206ffe95fb35365e16cae1ea7aa04af9
ChiaKi707 <1466491009@qq.com>
2025-12-06 13:38:27 +0800

    start

---
+++ src/main/java/cyber/hometown2/Hometown2Application.java
@@ -0,0 +1,13 @@
+package cyber.hometown2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hometown2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hometown2Application.class, args);
+    }
+
+}
